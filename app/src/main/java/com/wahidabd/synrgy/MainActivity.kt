c6c54aa9880b286b1387dbb5737ff55ea8830d18
Abd Wahid <wahed.blog99@gmail.com>
2023-09-30 19:52:30 +0700

    add: logic for handle calculate

--- app/src/main/java/com/wahidabd/synrgy/MainActivity.kt
+++ app/src/main/java/com/wahidabd/synrgy/MainActivity.kt
@@ -1,19 +1,45 @@
 package com.wahidabd.synrgy
 
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import com.wahidabd.synrgy.databinding.ActivityMainBinding
+import com.wahidabd.synrgy.utils.formatCurrencyRp
+import com.wahidabd.synrgy.utils.getPercentage
+import com.wahidabd.synrgy.utils.toCurrencyTextWatcher
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var percentage = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        handleSetSelectionRadioButton()
+
+        binding.edtInput.toCurrencyTextWatcher()
+        binding.btnCalculate.setOnClickListener { handleCalculate() }
     }
 
+    private fun handleCalculate() = with(binding) {
+        val input = edtInput.text?.toString()?.toDouble() ?: 0.0
+
+        val result =
+            if (switchButton.isChecked) (input + (input * percentage)).roundToInt().toDouble()
+            else input + (input * percentage)
+        tvResult.text = result.formatCurrencyRp()
+    }
+
+    private fun handleSetSelectionRadioButton() {
+        binding.rg.setOnCheckedChangeListener { _, id ->
+            val selected = findViewById<RadioButton>(id)
+            percentage = selected.text.toString().getPercentage()
+        }
+    }
 
 }
---
+++ app/src/main/java/com/wahidabd/synrgy/utils/FunctionExt.kt
@@ -0,0 +1,69 @@
+package com.wahidabd.synrgy.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import java.text.NumberFormat
+import java.util.Currency
+import java.util.Locale
+
+
+/**
+ * Created by Wahid on 9/30/2023.
+ * Github github.com/wahidabd.
+ */
+
+val localeIndonesia = Locale("in", "ID")
+
+
+fun EditText.toCurrencyTextWatcher() {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            s?.let { string ->
+                if (string.isNotEmpty()) {
+                    this@toCurrencyTextWatcher.removeTextChangedListener(this)
+
+                    val price = fromCurrencyStringToDouble(string.toString())
+                    val priceString = price.toCurrencyValue()
+
+                    this@toCurrencyTextWatcher.setText(priceString)
+                    this@toCurrencyTextWatcher.setSelection(priceString.length)
+                    this@toCurrencyTextWatcher.addTextChangedListener(this)
+                }
+            }
+        }
+
+    })
+}
+
+private fun Double.toCurrencyValue(): String {
+    val formatted = NumberFormat.getCurrencyInstance(localeIndonesia)
+    formatted.maximumFractionDigits = 0
+    val format = formatted.format(this)
+    return getNormalizedValue(format)
+}
+
+private fun fromCurrencyStringToDouble(currencyString: String): Double =
+    currencyString.replace(".", "").toDouble()
+
+
+private fun getNormalizedValue(value: String): String =
+    value.substring(2, value.length)
+
+fun Double.formatCurrencyRp(): String {
+    val format = NumberFormat.getCurrencyInstance(localeIndonesia)
+    return format.format(this)
+}
+
+fun String.getPercentage(): Double {
+    return when {
+        this.contains("20") -> 20.0 / 100.0
+        this.contains("18") -> 18.0 / 100.0
+        this.contains("15") -> 15.0 / 100.0
+        else -> 0.0
+    }
+}
